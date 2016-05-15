2efe31f9ddc1ab58ba816ff68556c600a45bc096
BRANDON BARRETT <mrbrandonbarrett@gmail.com>
2016-05-15 11:36:41 -0600

    refined camera code, started in on TCP work

--- app/src/main/java/com/robobrandon/opencvtest/MainActivity.java
+++ app/src/main/java/com/robobrandon/opencvtest/MainActivity.java
@@ -1,52 +1,156 @@
 package com.robobrandon.opencvtest;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
+
+
+    // Used for logging success or failure messages
+    private static final String TAG = "OCVSample::Activity";
+
+    // Loads camera view of OpenCV for us to use. This lets us see using OpenCV
+    // http://docs.opencv.org/java/2.4.8/org/opencv/android/CameraBridgeViewBase.html
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    // TODO: Add GUI interface for connecting to TCP server?
+    private static final int SERVER_PORT = 5000;
+    private static final String SERVER_IP = "10.0.2.2";
+
+    private Boolean sendingFrame = false;
+
+    // These variables are used (at the moment) to fix camera orientation from 270degree to 0degree
+    Mat mRgba;
+    Mat mRgbaF;
+    Mat mRgbaT;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    public MainActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.show_camera);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.show_camera_activity_java_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaF = new Mat(height, width, CvType.CV_8UC4);
+        mRgbaT = new Mat(width, width, CvType.CV_8UC4);
+    }
+
+    public void onCameraViewStopped() {
+//        mRgba.release();
+    }
+
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+
+//        // TODO Auto-generated method stub
+//        mRgba = inputFrame.rgba();
+//        // Rotate mRgba 90 degrees
+//        Core.transpose(mRgba, mRgbaT);
+//        Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(), 0,0, 0);
+//        Core.flip(mRgbaF, mRgba, 1);
+//
+//        return mRgba; // This function must return
+        sendFrameCapture(inputFrame.rgba());
+        return inputFrame.rgba();
+    }
+
+    public void triggerFrameCapture(){
+        sendingFrame = true;
+    }
+
+    private void sendFrameCapture(Mat capturedFrame){
+        // Can use this as a backup method...  save image and THEN stream
+        //Bitmap bm = Bitmap.createBitmap(capturedFrame.cols(), capturedFrame.rows(),Bitmap.Config.ARGB_8888);
+        //Utils.matToBitmap(capturedFrame, bm);
+
+        // Possible that I'll need to specify elemSize?
+        //int size = (int) capturedFrame.total() * capturedFrame.elemSize();
+        int size = (int) (capturedFrame.total());
+        byte[] buffer = new byte[size];
+
+//        with open("media/omg.png", "rb") as imageFile:
+//        f = imageFile.read()
+//        b = bytearray(f)
+//        newMsg = "SendingFrame,%d\n" % len(b)
+//        sock.sendall(newMsg)
+//
+//        sock.send(b)
+    }
+
 }
